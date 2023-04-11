773d141ea5816aab6e3be1ca2175ed777901b333
autorun <20293349@qq.com>
2023-04-11 16:08:28 +0800

    add method https://tour.go-zh.org/basics/9

--- src/main.go
+++ src/main.go
@@ -5,8 +5,14 @@ import (
 	"github.com/error0702/go-study/v2/src/hello"
 )
 
+// 成员变量
+var i, j int = 1, 2
+
 func main() {
 	//fmt.Println("hello world!")
 	hello.Print()
 	fmt.Println(hello.Split(10))
+	// 定义局部变量
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 }
