d0d8b375a7c2fa25a720bf1c485ccdf0c24e9725
autorun <20293349@qq.com>
2023-04-11 16:28:38 +0800

    add type.go https://tour.go-zh.org/basics/11

--- src/hello/type.go
+++ src/hello/type.go
@@ -5,6 +5,25 @@ import (
 	"math/cmplx"
 )
 
+/**
+基本类型：
+	bool
+
+	string
+
+	int  int8  int16  int32  int64
+	uint uint8 uint16 uint32 uint64 uintptr
+
+	byte // uint8 的别名
+
+	rune // int32 的别名
+    	// 表示一个 Unicode 码点
+
+	float32 float64
+
+	complex64 complex128
+*/
+
 var (
 	// 申明类型别名
 	ToBe   bool       = false
